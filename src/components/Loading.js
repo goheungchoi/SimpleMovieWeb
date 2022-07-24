c1df8f5534d1e15c762f1d51d4d1f144c8994eca
goheungchoi <choi0740@umn.edu>
2022-07-23 23:40:11 -0500

    Loading Screen
    Created a loading screen with CSS spinning animation and better design.

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,5 +1,20 @@
+import React from 'react'
+import style from './Loading.module.css'
+
 function Loading() {
-  return null;
+  return (
+    <div className={style.Body}>
+      <div className={style.Loading_screen_header}>
+        <span className={style.Header}>Loading</span>
+        <div className={style.lds_ring}>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Loading;
